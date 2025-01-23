4b2d00fd59e9a5dea090d05706f88311c8a05985
overtonjust <joverton@pursuit.org>
2025-01-22 20:54:28 -0500

    style: initial styling pass

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 // Dependencies
 import React from 'react';
+import './App.scss';
+import { Formik, Form } from 'formik';
 
 const App = () => {
   return (
-    <form>
-        <h2>Podcast Generator</h2>
-        <div>
-          <button>Upload Audio</button> 
-          <button>Enter Transcript</button>
+    <form className='form'>
+        <h1 className='form__header'>Podcast Generator</h1>
+        <div className='form__options'>
+          <button className='form__button'>Upload Audio</button> 
+          <button className='form__button blue'>Enter Transcript</button>
         </div>
-        <textarea name="" id="">Paste your transcript here...</textarea>
-        <button type="submit">Generate Podcast</button>
+        <textarea className='form__textarea' name="" id="">Paste your transcript here...</textarea>
+        <button className='form__submit' type="submit">Generate Podcast</button>
     </form>
   );
 
